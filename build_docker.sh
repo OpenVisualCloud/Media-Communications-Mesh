992b5fdb31d7cc26681b361481957796ea02cd78
Milosz Linkiewicz <milosz.linkiewicz@intel.com>
2024-06-05 04:11:53 +0000

    Common methods and scripting improved
    Common methods and scripting improved:
    - added file `common.sh` in root repository directory with some of commonly used functions.
    - added fail on error in every bash script, including pipefail
    - improved scripting by usage of absolute paths instead of relative ones (any directory removal with relative paths can have unexpected behaviour)
    - fixed bash script files with `#!/bin/bash` not in first line.
    
    Also `build_docker.sh` file was added with basic workflow of building media-proxy Dockefile with the same approach as mentioned above.
    - build docker files using buildx toolkit
    - read and pass possible proxy variables from environment
    
    Signed-off-by: Milosz Linkiewicz <milosz.linkiewicz@intel.com>

---
+++ build_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2024 Intel Corporation
+
+set -eo pipefail
+SCRIPT_DIR="$(readlink -f "$(dirname -- "${BASH_SOURCE[0]}")")"
+. "${SCRIPT_DIR}/common.sh"
+
+# Read proxy variables from env to pass them to the builder
+BUILD_ARGUMENTS=$(compgen -e | sed -nE '/_(proxy|PROXY)$/{s/^/--build-arg /;p}')
+
+docker buildx build --progress=plain --network=host ${BUILD_ARGUMENTS} -f "${SCRIPT_DIR}/media-proxy/Dockerfile" $@ "${SCRIPT_DIR}"
