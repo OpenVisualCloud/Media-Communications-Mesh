9c2569418ed76173119d263214d2c99fe637472e
Tomasz <tomasz.szumski@intel.com>
2024-08-30 13:37:50 +0200

    Cleanup ffmpeg plugin build scripts (#183)
    Jpeg-xs is not required as direct ffmpeg-plugin,
    Jpeg-xs is used internally by MTL (kahawai.json), inside media-proxy

--- ffmpeg-plugin/configure-ffmpeg.sh
+++ ffmpeg-plugin/configure-ffmpeg.sh
@@ -9,24 +9,13 @@ SCRIPT_DIR="$(readlink -f "$(dirname -- "${BASH_SOURCE[0]}")")"
 BUILD_DIR="${BUILD_DIR:-${SCRIPT_DIR}/build}"
 . "${SCRIPT_DIR}/../common.sh"
 
-JPEGXS_FLAGS=( )
-JPEGXS_ENABLED="${JPEGXS_ENABLED:-1}"
-
-if [ "$JPEGXS_ENABLED" == "1" ]
-then
-    pushd "${BUILD_DIR}/jpegxs/Build/linux"
-    run_as_root_user ./build.sh release install
-    JPEGXS_FLAGS+=( "--enable-libsvtjpegxs" )
-    popd
-fi
-
 pushd "${BUILD_DIR}/FFmpeg"
 PKG_CONFIG_PATH="/usr/local/lib/pkgconfig" pkg-config --exists --print-errors libmcm_dp
 
 # copy source files to allow the configure tool to find them
 #cp -f ../mcm_* ./libavdevice/
 
-"${BUILD_DIR}/FFmpeg/configure" --enable-shared --enable-mcm ${JPEGXS_FLAGS[@]} $@
+"${BUILD_DIR}/FFmpeg/configure" --enable-shared --enable-mcm $@
 popd
 
 prompt "FFmpeg MCM plugin configuration completed."
