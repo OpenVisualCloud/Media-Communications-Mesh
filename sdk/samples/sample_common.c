f135916bee405662ceac32169da01209e5370ec7
gta <mateusz.grabuszynski@intel.com>
2024-09-10 13:27:40 +0000

    Changing the way frame size is determined for st30/audio

--- sdk/samples/sample_common.c
+++ sdk/samples/sample_common.c
@@ -29,47 +29,54 @@ static int getAudioFrameSize(
     mcm_audio_ptime ptime,
     uint32_t audio_channels)
 {
-    // AUDIO_PTIME_1_09MS, /**< packets time of 1.09ms, only for 44.1kHz sample */
-    // AUDIO_PTIME_0_14MS, /**< packet time of 0.14ms, only for 44.1kHz sample */
-    // AUDIO_PTIME_0_09MS, /**< packet time of 0.09ms, only for 44.1kHz sample */
-
     // Audio frame size
     int bits_per_sample;
     int samples_per_second;
     int slices_per_second;
 
     // mcm_audio_format
-    if (audio_fmt == AUDIO_FMT_PCM8){
-        bits_per_sample = 8;
-    } else if (audio_fmt == AUDIO_FMT_PCM24){
-        bits_per_sample = 24;
-    } else if (audio_fmt == AUDIO_FMT_AM824){
-        bits_per_sample = 32;
-    } else { //if (audio_fmt == AUDIO_FMT_PCM16)
-        bits_per_sample = 16;
-    }
+    bits_per_sample = (audio_fmt + 1);
 
-    // for 48k
-    if (ptime == AUDIO_PTIME_125US){
-        slices_per_second = 8000; // 6
-    } else if (ptime == AUDIO_PTIME_250US){
-        slices_per_second = 4000; // 12
-    } else if (ptime == AUDIO_PTIME_4MS){
-        slices_per_second = 250; // 192
-    } else { // if (ptime == AUDIO_PTIME_1MS)
-        slices_per_second = 1000; // 48 samples per slice
+    switch (sampling){
+        case AUDIO_SAMPLING_96K:
+        case AUDIO_SAMPLING_48K:
+            switch (ptime) {
+            case AUDIO_PTIME_250US:
+                slices_per_second = 4000;
+                break;
+            case AUDIO_PTIME_4MS:
+                slices_per_second = 250;
+                break;
+            case AUDIO_PTIME_333US:
+                slices_per_second = 3003;
+                break;
+            default:
+                slices_per_second = 1;
+                break;
+            }
+        case AUDIO_SAMPLING_44K:
+            switch (ptime) {
+            case AUDIO_PTIME_1MS:
+            case AUDIO_PTIME_1_09MS: // 44100 Hz
+                slices_per_second = 1000;
+                break;
+            case AUDIO_PTIME_125US:
+            case AUDIO_PTIME_0_14MS: // 44100 Hz
+                slices_per_second = 8000;
+                break;
+            case AUDIO_PTIME_80US:
+            case AUDIO_PTIME_0_09MS: // 44100 Hz
+                slices_per_second = 12500;
+                break;
+            default:
+                slices_per_second = 1;
+                break;
+            }
+            break;
+        default:
+            slices_per_second = 1;
+            break;
     }
-    // } else if (ptime == AUDIO_PTIME_333US){
-    //     slices_per_second = 3003; // TODO: not ideal
-    // } else if (ptime == AUDIO_PTIME_80US){
-    //     slices_per_second = 12500; // 0.384?
-    // } else if (ptime == AUDIO_PTIME_1_09MS){
-    //     slices_per_second = 917; // TODO: not ideal
-    // } else if (ptime == AUDIO_PTIME_0_14MS){
-    //     slices_per_second = 7142; // TODO: not ideal
-    // } else if (ptime == AUDIO_PTIME_0_09MS){
-    //     slices_per_second = 11111; // TODO: not ideal
-    // }
 
     if (sampling == AUDIO_SAMPLING_96K){
         samples_per_second = 96000;
