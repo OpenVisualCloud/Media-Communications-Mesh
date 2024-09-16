db00fd9edd585a13b29cdf9754cc20ee7f4724e5
Kasiewicz, Marek <marek.kasiewicz@intel.com>
2024-09-16 12:21:50 +0000

    Add format-coding.sh
    Change .clang-format to be similar to linux kernel
    code style
    
    Signed-off-by: Kasiewicz, Marek <marek.kasiewicz@intel.com>

---
+++ format-coding.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2022 Intel Corporation
+
+# Based on clang-format
+# For ubuntu, pls "apt-get install clang-format"
+
+set -e
+
+echo "clang-format check"
+find . \( -path ./out -o -path ./ffmpeg-plugin \) -prune -o -regex '.*\.\(cpp\|hpp\|cc\|c\|h\)' \
+	-exec clang-format --verbose -i {} +
