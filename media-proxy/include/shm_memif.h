a4ca284bd2532a95237e5f485a11c73230d84a3b
Marek Kasiewicz <marek.kasiewicz@intel.com>
2024-09-20 15:23:19 +0200

    Make memif_buffer_alloc_timeout() more readable
    Co-authored-by: Konstantin Ilichev <konstantin.ilichev@intel.com>
    Signed-off-by: Kasiewicz, Marek <marek.kasiewicz@intel.com>

--- media-proxy/include/shm_memif.h
+++ media-proxy/include/shm_memif.h
@@ -88,7 +88,7 @@ int tx_rdma_on_receive(memif_conn_handle_t conn, void *priv_data, uint16_t qid);
 
 int memif_buffer_alloc_timeout(memif_conn_handle_t conn, uint16_t qid,
                                memif_buffer_t * bufs, uint16_t count, uint16_t * count_out,
-                               uint32_t size, unsigned int timeout_ms);
+                               uint32_t size, uint32_t timeout_ms);
 
 #ifdef __cplusplus
 }
