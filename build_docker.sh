ea65dccc7c3574f34a31a19ad5371f6a6f5ecfda
Milosz Linkiewicz <milosz.linkiewicz@intel.com>
2024-10-07 15:59:11 +0000

    Dockerfiles and build script improved
    Improvement: Dockerfiles and build script
    - minor functional improvement removing configuration steps
    - patch apply method made more robust (generic `patch` instead of `git am`)
    - build script now include both 6.1 and 7.0 versions, `7.0` being tagged as latest
    - ffmpeg docker versioning variable added for easy runtime identyfication
    
    Signed-off-by: Milosz Linkiewicz <milosz.linkiewicz@intel.com>

--- build_docker.sh
+++ build_docker.sh
@@ -13,10 +13,15 @@ IMAGE_TAG="${IMAGE_TAG:-latest}"
 
 # Read proxy variables from env to pass them to the builder
 BUILD_ARGUMENTS=()
-BASIC_ARGUMENTS=("buildx" "build" "--progress=plain" "--network=host" "--build-arg=IMAGE_CACHE_REGISTRY=${IMAGE_CACHE_REGISTRY}")
 mapfile -t BUILD_ARGUMENTS < <(compgen -e | sed -nE '/^(.*)(_proxy|_PROXY)$/{s/^/--build-arg=/;p}')
+BUILD_ARGUMENTS+=( "--progress=plain" "--network=host" "--build-arg IMAGE_CACHE_REGISTRY=${IMAGE_CACHE_REGISTRY}" )
 
+docker buildx build "${BUILD_ARGUMENTS[@]}" -t "${IMAGE_REGISTRY}/mcm/sdk:${IMAGE_TAG}" -f "${SCRIPT_DIR}/sdk/Dockerfile" "$@" "${SCRIPT_DIR}"
+docker buildx build "${BUILD_ARGUMENTS[@]}" -t "${IMAGE_REGISTRY}/mcm/media-proxy:${IMAGE_TAG}" -f "${SCRIPT_DIR}/media-proxy/Dockerfile" "$@" "${SCRIPT_DIR}"
+docker buildx build "${BUILD_ARGUMENTS[@]}" --build-arg FFMPEG_VER="7.0" -t "${IMAGE_REGISTRY}/mcm/ffmpeg:${IMAGE_TAG}" -f "${SCRIPT_DIR}/ffmpeg-plugin/Dockerfile" "$@" "${SCRIPT_DIR}"
+docker buildx build "${BUILD_ARGUMENTS[@]}" --build-arg FFMPEG_VER="6.1" -t "${IMAGE_REGISTRY}/mcm/ffmpeg:6.1-${IMAGE_TAG}" -f "${SCRIPT_DIR}/ffmpeg-plugin/Dockerfile" "$@" "${SCRIPT_DIR}"
 
-docker tag "${IMAGE_REGISTRY}/sdk:${IMAGE_TAG}" "mcm/sample-app:${IMAGE_TAG}"
-docker tag "${IMAGE_REGISTRY}/media-proxy:${IMAGE_TAG}" "mcm/media-proxy:${IMAGE_TAG}"
-docker tag "${IMAGE_REGISTRY}/ffmpeg:${IMAGE_TAG}" "mcm/ffmpeg:${IMAGE_TAG}"
+docker tag "${IMAGE_REGISTRY}/mcm/sdk:${IMAGE_TAG}" "mcm/sample-app:${IMAGE_TAG}"
+docker tag "${IMAGE_REGISTRY}/mcm/media-proxy:${IMAGE_TAG}" "mcm/media-proxy:${IMAGE_TAG}"
+docker tag "${IMAGE_REGISTRY}/mcm/ffmpeg:${IMAGE_TAG}" "mcm/ffmpeg:${IMAGE_TAG}"
+docker tag "${IMAGE_REGISTRY}/mcm/ffmpeg:6.1-${IMAGE_TAG}" "mcm/ffmpeg:6.1-${IMAGE_TAG}"
