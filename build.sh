f57c0c1ff41ecfe30e91d6737db846ae51d2d930
Milosz Linkiewicz <milosz.linkiewicz@intel.com>
2024-10-10 13:15:22 +0000

    Scripting improvements and scripts robustnes
    Scripting improvements and scripts robustnes.
    - More methods moved to generic common.sh
    - setup_build_env.sh made smaller.
    - setup_rdma_env.sh fixed potential dangerous rm -rf calls
    - many small fixes and shellcheck compliance adjustments.
    
    Installation steps change of order:
    - Move irdma installation after ice installation.
    - Move libfabric installation after irdma installation
    
    Signed-off-by: Milosz Linkiewicz <milosz.linkiewicz@intel.com>

--- build.sh
+++ build.sh
@@ -5,23 +5,25 @@
 
 set -eo pipefail
 SCRIPT_DIR="$(readlink -f "$(dirname -- "${BASH_SOURCE[0]}")")"
+
 . "${SCRIPT_DIR}/scripts/common.sh"
 
 # Set build type. ("Debug" or "Release")
-BUILD_TYPE="${BUILD_TYPE:-Release}"
 # To disable the building of unit tests, set the value to "OFF".
+
+BUILD_TYPE="${BUILD_TYPE:-Release}"
 BUILD_UNIT_TESTS="${BUILD_UNIT_TESTS:-ON}"
 INSTALL_PREFIX="${INSTALL_PREFIX:-/usr/local}"
 
-cmake -B "${SCRIPT_DIR}/out" \
+cmake -B "${BUILD_DIR}" \
     -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
     -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
     -DBUILD_UNIT_TESTS="${BUILD_UNIT_TESTS}" \
     "${SCRIPT_DIR}"
-cmake --build "${SCRIPT_DIR}/out" -j
+cmake --build "${BUILD_DIR}" -j
 
 # Install
-run_as_root_user cmake --install "${SCRIPT_DIR}/out"
+run_as_root_user cmake --install "${BUILD_DIR}"
 run_as_root_user ln -s /usr/lib64/libbpf.so.1 /usr/lib/x86_64-linux-gnu/libbpf.so.1 2>/dev/null || true
 run_as_root_user ldconfig
 
--- scripts/setup_rdma_env.sh
+++ scripts/setup_rdma_env.sh
@@ -8,60 +8,53 @@ WORKING_DIR="${BUILD_DIR:-${REPO_DIR}/build/rdma}"
 
 . "${SCRIPT_DIR}/common.sh"
 
-IRDMA_DMID="832291"
-IRDMA_VER="1.15.11"
+LIBFABRIC_VER="${LIBFABRIC_VER:-v1.22.0}"
 
-if [[ -f $WORKING_DIR ]]; then
-    error "Can't create rdma directory because of the rdma file $WORKING_DIR"
-    exit 1
-fi
-
-mkdir -p "$WORKING_DIR"
-
-install_irdma() {
-    echo "Installing irdma driver..."
-    pushd "$WORKING_DIR"
-    wget "https://downloadmirror.intel.com/${IRDMA_DMID}/irdma-${IRDMA_VER}.tgz" -O ./irdma.tar.gz
-    tar -xf ./irdma.tar.gz
-    pushd "./irdma-${IRDMA_VER}"
+function install_irdma() {
+    prompt "Start of install_irdma method. iRDMA installation starting."
+    NO_PROXY="" no_proxy="" \
+    wget_download_strip_unpack "https://downloadmirror.intel.com/${IRDMA_DMID}/irdma-${IRDMA_VER}.tgz" "${WORKING_DIR}/irdma-${IRDMA_VER}"
+    pushd "${WORKING_DIR}/irdma-${IRDMA_VER}"
     sudo ./build.sh
     popd
-    popd
-    modprobe irdma
+    sudo modprobe irdma
+    prompt "End of install_irdma method. iRDMA installation successful."
 }
 
-install_perftest() {
-    echo "Installing perftest dependencies..."
+function install_perftest() {
+    prompt "Start of install_perftest method. Installing apt packages."
     sudo apt update
     sudo apt install -y libibverbs-dev librdmacm-dev libibumad-dev libpci-dev
-    echo "Installing perftest..."
-    pushd "$WORKING_DIR"
-    wget https://github.com/linux-rdma/perftest/releases/download/24.07.0-0.44/perftest-24.07.0-0.44.g57725f2.tar.gz -O perftest.tar.gz
-    tar -xf perftest.tar.gz
-    pushd ./perftest-24.07.0
+    prompt "Install_perftest method. Installing apt packages successful."
+
+    prompt "Install_perftest method. Downloading and installing perftest-24.07.0."
+    wget_download_strip_unpack https://github.com/linux-rdma/perftest/releases/download/24.07.0-0.44/perftest-24.07.0-0.44.g57725f2.tar.gz "${WORKING_DIR}/perftest-24.07.0"
+
+    pushd "${WORKING_DIR}/perftest-24.07.0"
     ./autogen.sh
     ./configure
-    make -j$(nproc)
+    make -j "${NPROC}"
     sudo make install
     popd
-    popd
+    prompt "End of install_perftest method. Finished installing perftest-24.07.0."
 }
 
-install_libfabric() {
-    echo "Installing libfabric from the source..."
-    pushd "$WORKING_DIR"
-    git clone --depth 1 --branch v1.22.0 https://github.com/ofiwg/libfabric libfabric
-    pushd libfabric
-    ./autogen.sh
-    ./configure --enable-verbs
-    make -j$(nproc)
+
+function install_libfabric() {
+    prompt "Start of install_libfabric method. Installing libfabric from source."
+    git_download_strip_unpack "ofiwg/libfabric" "${LIBFABRIC_VER}" "${WORKING_DIR}/libfabric"
+
+    pushd "${WORKING_DIR}/libfabric"
+    ./autogen.sh && \
+    ./configure --enable-verbs && \
+    make -j "${NPROC}" && \
     sudo make install
     sudo ldconfig
     popd
-    popd
+    prompt "End of install_libfabric method. Finished installing libfabric from source."
 }
 
-check_roce() {
+function check_roce() {
     echo -n "Checking RoCE... "
     roce_ena_val=$(cat /sys/module/irdma/parameters/roce_ena)
     if [[ "$roce_ena_val" != "1" ]] && [[ "$roce_ena_val" != "65535" ]]; then
@@ -71,7 +64,7 @@ check_roce() {
     echo "OK"
 }
 
-check_mtu() {
+function check_mtu() {
     echo "Checking MTU..."
     rdma_devices=($(rdma link | grep "LINK_UP" | cut -d " " -f 8))
     if [[ "${#rdma_devices[@]}" == "0" ]]; then
@@ -93,13 +86,13 @@ check_mtu() {
     fi
 }
 
-set_mtu() {
+function set_mtu() {
     echo -n "Setting MTU to 9000 on $1... "
     sudo ip link set dev $1 mtu 9000
     echo "DONE"
 }
 
-run_perftest() {
+function run_perftest() {
     # Usage: run_perftest <interface_name> [network_address] [network_mask]
     interface_name="$1"
     network_address="${2:-192.168.255.255}"
@@ -116,14 +109,22 @@ run_perftest() {
     sudo ip addr del ${network_address}/${network_mask} dev ${interface_name}
 }
 
+if [[ -f $WORKING_DIR ]]; then
+    error "Can't create rdma directory because of the rdma file $WORKING_DIR"
+    exit 1
+fi
+mkdir -p "$WORKING_DIR"
+
 if [[ "$1" == "install" ]]; then
-    rm -rf "$WORKING_DIR"/*
+    rm -rf "${WORKING_DIR}" && \
+    mkdir -p "${WORKING_DIR}" && \
     install_irdma
     config_intel_rdma_driver
     install_libfabric
-    echo "Reboot required"
+    warning "For changes to take place, an OS reboot is required."
 elif [[ "$1" == "install_perftest" ]]; then
-    rm -rf "$WORKING_DIR"/*
+    rm -rf "${WORKING_DIR}" && \
+    mkdir -p "${WORKING_DIR}" && \
     install_perftest
 elif [[ "$1" == "check" ]]; then
     check_roce
