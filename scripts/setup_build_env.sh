cb8b256b5a26481d0074a95a2d52a911502eae89
Miłosz Linkiewicz <milosz.linkiewicz@intel.com>
2024-11-07 23:26:06 +0100

    FIX: Add required ice and irdma packages
    FIX: Add required packages for ice and irdma modules to work. Updated setup_build_env.sh
    
    Signed-off-by: Miłosz Linkiewicz <milosz.linkiewicz@intel.com>

--- scripts/setup_build_env.sh
+++ scripts/setup_build_env.sh
@@ -58,6 +58,7 @@ function install_ubuntu_package_dependencies()
 {
     set -x
     APT_LINUX_HEADERS="linux-headers-$(uname -r)"
+    APT_LINUX_MOD_EXTRA="linux-modules-extra-$(uname -r)"
     apt-get update --fix-missing && \
     apt-get install --no-install-recommends -y \
         apt-transport-https \
@@ -96,7 +97,10 @@ function install_ubuntu_package_dependencies()
         systemtap-sdt-dev \
         wget \
         zlib1g-dev \
-        "${APT_LINUX_HEADERS}"
+        v4l2loopback-dkms \
+        v4l2loopback-utils \
+        "${APT_LINUX_HEADERS}" \
+        "${APT_LINUX_MOD_EXTRA}"
     set +x
     return 0
 }
