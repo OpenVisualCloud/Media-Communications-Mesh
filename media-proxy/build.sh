c2f586c7825931470f2074f98bb2a0baed4d2c1e
Miłosz Linkiewicz <milosz.linkiewicz@intel.com>
2024-11-27 14:20:35 +0100

    FIXED: Full working gha type docker cache (#267)
    FIXED: Full working GHA type docker cache
    - unification of repository workflow actions
    - removed and merged unnecessary files in workflows
    - added concurrency sub-key in each workflow for full workflow action to be re-run on content changed.
    - Trivy scans are made now on each build docker image
    - added build distinction for FFmpeg 6.1 and 7.0
    - many more less significant changes.
    
    ADDED: Version file and bash code fixes added.
    - All version information have been moved to same file as in SDB `versions.env`
    - FIX: FFmpeg scripts have been modified to allow easy change of base FFmpeg version
    - FIX: Bash methods for printing to std::out and std::err renamed to not shadow other useful ones like `error`.
    - FIX: method for root check renamed and fixed - from `run_as_root_user` to `as_root`
    - FIX: Color handling inside `common.sh` - simplified and fixed.
    - UPDATED: new names for printing `log_info`, `log_warning`, `log_error`, `log_message`.
    - ADDED: method for success printing, `log_success`
    - FIXED: building scripts have been modified and unified.
    
    
    UPDATED: CiCd Task for CodeQL, build bare-metal Ubuntu:
    - FIX: CodeQL task build time and used script.
    - All tasks should and will use this approach - same script and methods must be used for valid checks outcome.
    - FIXED: CMakeLists.txt to treat sample apps as binary runtime targets
    - ADDED: `_build` and `_install` directories to `.dockerignore`
    - FIXED: SDK build.sh script to work as intended
    - FIXED to use script instead of custom build steps task build time and used script.
    - Minor naming convention changes
    
    UPDATED: Build bare metal ubuntu workflow
    - Fixed/changed to use the same script as other jobs instead of using custom build steps
    - Updated: Minor naming convention changes were made
    
    Signed-off-by: Milosz Linkiewicz <milosz.linkiewicz@intel.com>
    
    ADDED: Hadolint static code scans to repository:
    - All dockerfiles are being scanned using Hadolint before build starts. 
    - Hadolint upload all issues to security tab.
    - Hadolint mark errors in-place using "File changed" tab
    
    Co-authored-by: Lukasz Grab <lukasz.grab@intel.com>
    Co-authored-by: Milosz Linkiewicz <milosz.linkiewicz@intel.com>
    
    ---------
    
    Signed-off-by: Milosz Linkiewicz <milosz.linkiewicz@intel.com>

--- media-proxy/build.sh
+++ media-proxy/build.sh
@@ -3,21 +3,26 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright 2024 Intel Corporation
 
+# Passing custom path as a parameter to this script will install
+# SDK in to default path and the one pointed as $1
+
 set -eo pipefail
 SCRIPT_DIR="$(readlink -f "$(dirname -- "${BASH_SOURCE[0]}")")"
 REPO_DIR="$(readlink -f "${SCRIPT_DIR}/..")"
 
+# shellcheck source="../scripts/common.sh"
 . "${REPO_DIR}/scripts/common.sh"
 
+MCM_MEDIA_PROXY_DIR="${MCM_MEDIA_PROXY_DIR:-${BUILD_DIR}/mcm-media-proxy}"
 # Set build type. ("Debug" or "Release")
 BUILD_TYPE="${BUILD_TYPE:-Release}"
 INSTALL_PREFIX="${INSTALL_PREFIX:-/usr/local}"
 
-cmake -B "${SCRIPT_DIR}/out" \
-    -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
-    -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
-    "${SCRIPT_DIR}"
-cmake --build "${SCRIPT_DIR}/out" -j
+cmake -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -B "${MCM_MEDIA_PROXY_DIR}" -S "${SCRIPT_DIR}"
 
-# Install
-run_as_root_user cmake --install "${SCRIPT_DIR}/out"
+as_root make -C "${MCM_MEDIA_PROXY_DIR}" install
+
+if [[ $# -ne 0 ]]; then
+    DESTDIR="${1:-$DESTDIR}" make -C "${MCM_MEDIA_PROXY_DIR}" install
+fi
+ln -s "${MCM_MEDIA_PROXY_DIR}" "${SCRIPT_DIR}/build"
--- scripts/setup_build_env.sh
+++ scripts/setup_build_env.sh
@@ -1,95 +1,51 @@
 #!/bin/bash
 
 set -eEo pipefail
-set +x
 
 SCRIPT_DIR="$(readlink -f "$(dirname -- "${BASH_SOURCE[0]}")")"
-REPO_DIR="$(readlink -f "${SCRIPT_DIR}/..")"
-
 . "${SCRIPT_DIR}/common.sh"
 
 export PM="${PM:-apt-get}"
-export MCM_DIR="${BUILD_DIR}/mcm"
-export MTL_VER="${MTL_VER:-maint-24.09}" # maint-24.09
-export MTL_DIR="${BUILD_DIR}/mtl"
-export MTL_REPO="${MTL_REPO:-OpenVisualCloud/Media-Transport-Library}"
-export DPDK_VER="${DPDK_VER:-23.11}"
-export DPDK_DIR="${BUILD_DIR}/dpdk"
-export XDP_VER="${XDP_VER:-d7edea3590052581c5fda5f8cfa40ae7be94f05c}"
-export XDP_DIR="${BUILD_DIR}/xdp"
-export BPF_VER="${BPF_VER:-42065ea6627ff6e1ab4c65e51042a70fbf30ff7c}"
-export BPF_DIR="${XDP_DIR}/lib/libbpf"
-export GPRC_VER="${GPRC_VER:-v1.58.0}"
-export GRPC_DIR="${BUILD_DIR}/grpc"
-export JPEGXS_VER="${JPEGXS_VER:-e1030c6c8ee2fb05b76c3fa14cccf8346db7a1fa}"
-export JPEGXS_DIR="${BUILD_DIR}/jpegxs"
-export LIBFABRIC_VER="${LIBFABRIC_VER:-v1.22.0}"
-export LIBFABRIC_DIR="${BUILD_DIR}/libfabric"
+export DEBIAN_FRONTEND="noninteractive"
 
-nproc="$(nproc)"
-export MAKEFLAGS="-j${nproc:-100}"
+export KERNEL_VERSION="${KERNEL_VERSION:-$(uname -r)}"
+export INSTALL_USE_CUSTOM_PATH="${INSTALL_USE_CUSTOM_PATH:-false}"
 
-KERNEL_VERSION="$(uname -r)"
-export KERNEL_VERSION
-export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/usr/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/lib/x86_64-linux-gnu/pkgconfig"
-export NASM_RPM_LINK="https://rpmfind.net/linux/centos-stream/9-stream/CRB/x86_64/os/Packages/nasm-2.15.03-7.el9.x86_64.rpm"
-export LIBFDT_DIR="${BUILD_DIR}/libfdt"
-export LIBFDT_REPO="https://github.com/dgibson/dtc/archive/refs/tags/v1.7.2.tar.gz"
-export JSONC_DIR="${BUILD_DIR}/json-c"
-export JSONC_REPO="https://github.com/json-c/json-c/archive/refs/tags/json-c-0.18-20240915.tar.gz"
-export LIBSDL2_DIR="${BUILD_DIR}/libsdl2"
-export LIBSDL2_REPO="https://github.com/libsdl-org/SDL/releases/download/release-2.30.9/SDL2-2.30.9.tar.gz"
+SYS_PKG_CONFIG_PATH="${SYS_PKG_CONFIG_PATH:-/usr/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/lib/x86_64-linux-gnu/pkgconfig}"
+if [[ "${PKG_CONFIG_PATH}" != *"${SYS_PKG_CONFIG_PATH}"* ]]; then
+    export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${SYS_PKG_CONFIG_PATH}"
+fi
 
-function get_and_patch_intel_drivers()
-{
-    if [ ! -d "${MTL_DIR}/patches/ice_drv/${ICE_VER}/" ]; then
-        error "MTL patch for ICE=v${ICE_VER} could not be found: ${MTL_DIR}/patches/ice_drv/${ICE_VER}"
-        return 1
-    fi
-    wget_download_strip_unpack "${IRDMA_REPO}" "${IRDMA_DIR}"
-    git_download_strip_unpack "intel/ethernet-linux-iavf" "refs/tags/v${IAVF_VER}" "${IAVF_DIR}"
-    git_download_strip_unpack "intel/ethernet-linux-ice"  "refs/tags/v${ICE_VER}"  "${ICE_DIR}"
-
-    pushd "${ICE_DIR}"
-    patch -p1 -i <(cat "${MTL_DIR}/patches/ice_drv/${ICE_VER}/"*.patch)
-    popd
-}
-
-function build_install_and_config_intel_drivers()
-{
-    make -j "${NPROC}" -C "${IAVF_DIR}/src" install && \
-    make -j "${NPROC}" -C "${ICE_DIR}/src" install && \
-    return 0 || return 1
-}
-
-function build_install_and_config_irdma_drivers()
-{
-    pushd "${IRDMA_DIR}" && \
-    ./build.sh && \
-    popd && \
-    config_intel_rdma_driver && \
-    modprobe irdma && \
-    return 0 || return 1
-}
+mkdir -p "${PREFIX_DIR}/usr/lib/x86_64-linux-gnu" \
+         "${PREFIX_DIR}/usr/local/lib/x86_64-linux-gnu" \
+         "${PREFIX_DIR}/usr/local/include" \
+         "${PREFIX_DIR}/usr/include"
 
 function install_package_dependencies()
 {
+    log_info Starting: OS packages installation.
+    log_info Starting: OS package manager auto-detection.
     setup_package_manager "apt-get"
     if [[ "${PM}" == "apt" || "${PM}" == "apt-get" ]]; then
+        log_success "Found ${PM}. Using Ubuntu package dependencies approach"
         install_ubuntu_package_dependencies
     elif [[ "${PM}" == "yum" || "${PM}" == "dnf" ]]; then
+        log_success "Found ${PM}. Using CentOS package dependencies approach"
         install_yum_package_dependencies
     else
-        error No supported package manager found
+        log_error "Exiting: No supported package manager found. Contact support"
         exit 1
     fi
+    log_info "Finished: Successful OS packages installation."
+    log_warning OS reboot is required for all of the changes to take place.
+    return 0
 }
 function install_ubuntu_package_dependencies()
 {
     APT_LINUX_HEADERS="linux-headers-${KERNEL_VERSION}"
     APT_LINUX_MOD_EXTRA="linux-modules-extra-${KERNEL_VERSION}"
-    "${PM}" update --fix-missing && \
-    "${PM}" install --no-install-recommends -y \
+    as_root "${PM}" update --fix-missing && \
+    as_root "${PM}" install --no-install-recommends -y \
         apt-transport-https \
         autoconf \
         automake \
@@ -128,18 +84,18 @@ function install_ubuntu_package_dependencies()
         wget \
         zlib1g-dev \
         "${APT_LINUX_HEADERS}" \
-        "${APT_LINUX_MOD_EXTRA}"
-    return 0
+        "${APT_LINUX_MOD_EXTRA}" && \
+    return 0 || return 1
 }
 
 function install_yum_package_dependencies()
 {
-    "${PM}" -y update
-    "${PM}" -y install epel-release
-    "${PM}" -y makecache --refresh
-    "${PM}" -y distro-sync
-    "${PM}" -y group install "Development Tools"
-    "${PM}" -y install \
+    as_root "${PM}" -y update && \
+    as_root "${PM}" -y install epel-release && \
+    as_root "${PM}" -y makecache --refresh && \
+    as_root "${PM}" -y distro-sync && \
+    as_root "${PM}" -y group install "Development Tools" && \
+    as_root "${PM}" -y install \
         autoconf \
         automake \
         bzip2 \
@@ -187,74 +143,102 @@ function install_yum_package_dependencies()
         texinfo \
         wget \
         zlib-devel && \
-    wget --tries=5 --progress=dot:giga "${NASM_RPM_LINK}" && \
-    "${PM}" -y localinstall nasm-*.rpm && \
-    rm -f "${NASM_RPM_LINK}" && \
     python3 -m pip install meson ninja && \
-    mkdir -p "${LIBFDT_DIR}" "${JSONC_DIR}" "${LIBSDL2_DIR}" && \
-    curl -Lf "${LIBFDT_REPO}" | tar -zx --strip-components=1 -C "${LIBFDT_DIR}" && \
-    make -C "${LIBFDT_DIR}" install && \
-    curl -Lf "${JSONC_REPO}" | tar -zx --strip-components=1 -C "${JSONC_DIR}" && \
-    mkdir -p "${JSONC_DIR}/json-c-build" && \
-    cmake -S "${JSONC_DIR}" -B "${JSONC_DIR}/json-c-build" -DCMAKE_BUILD_TYPE=Release && \
-    make -C "${JSONC_DIR}/json-c-build" install && \
+    lib_install_nasm_from_rpm && \
+    lib_build_and_install_libfdt && \
+    lib_build_and_install_jsonc && \
     return 0 || return 1
+}
+
+# Download and unpack dependencies from source code.
+function get_download_unpack_dependencies()
+{
+    mkdir -p "${GRPC_DIR}"
+    if ! git -C "${GRPC_DIR}" log -g --abbrev-commit --pretty=oneline HEAD "refs/tags/${GPRC_VER}" > /dev/null; then
+        git clone --branch "${GPRC_VER}" --depth 1 --recurse-submodules --shallow-submodules https://github.com/grpc/grpc "${GRPC_DIR}"
+    fi
+    git_download_strip_unpack "xdp-project/xdp-tools" "${XDP_VER}" "${XDP_DIR}"
+    git_download_strip_unpack "libbpf/libbpf" "${BPF_VER}" "${BPF_DIR}"
+    git_download_strip_unpack "ofiwg/libfabric" "${LIBFABRIC_VER}" "${LIBFABRIC_DIR}"
+    git_download_strip_unpack "OpenVisualCloud/Media-Transport-Library" "${MTL_VER}" "${MTL_DIR}"
+    git_download_strip_unpack "dpdk/dpdk" "refs/tags/v${DPDK_VER}" "${DPDK_DIR}"
+    git_download_strip_unpack "OpenVisualCloud/SVT-JPEG-XS" "${JPEGXS_VER}" "${JPEGXS_DIR}"
+    patch -d "${DPDK_DIR}" -p1 -i <(cat "${MTL_DIR}/patches/dpdk/${DPDK_VER}/"*.patch)
+}
+
+# Download and install rpm repo for nasm
+function lib_install_nasm_from_rpm()
+{
+    mkdir -p "${NASM_DIR}"
+    curl -Lf "${NASM_RPM_LINK}" -o "${NASM_DIR}/nasm-2.rpm"
+    as_root "${PM}" -y localinstall "${NASM_DIR}/nasm-"*.rpm
+}
+
+# Build and install libfdt from source code
+function lib_build_and_install_libfdt()
+{
+    git_download_strip_unpack "dgibson/dtc" "refs/tags/${LIBFDT_VER}" "${LIBFDT_DIR}"
+    make -j "${NPROC}" -C "${LIBFDT_DIR}"
+    as_root make -j "${NPROC}" -C "${LIBFDT_DIR}" install
+}
+
+# Build and install json-c from source code
+function lib_build_and_install_jsonc()
+{
+    git_download_strip_unpack "json-c/json-c/archive" "refs/tags/json-c-${JSON_C_VER}" "${JSONC_DIR}"
+    mkdir -p "${JSONC_DIR}/json-c-build"
+    cmake -S "${JSONC_DIR}" -B "${JSONC_DIR}/json-c-build" -DCMAKE_BUILD_TYPE=Release
+    make -j "${NPROC}" -C "${JSONC_DIR}/json-c-build"
+    as_root make -j "${NPROC}" -C "${JSONC_DIR}/json-c-build" install
 }
 
 # Build the xdp-tools project with ebpf
 function lib_install_xdp_bpf_tools()
 {
-    git_download_strip_unpack "xdp-project/xdp-tools" "${XDP_VER}" "${XDP_DIR}" && \
-    git_download_strip_unpack "libbpf/libbpf" "${BPF_VER}" "${BPF_DIR}" && \
     pushd "${XDP_DIR}" && \
     ./configure && \
-    make && \
-    make install && \
-    make -C "${XDP_DIR}/lib/libbpf/src" && \
-    make -C "${XDP_DIR}/lib/libbpf/src" install && \
+    make -j "${NPROC}" && \
+    as_root make -j "${NPROC}" install && \
+    DESTDIR="${PREFIX_DIR}" make -j "${NPROC}" install && \
+    make -j "${NPROC}" -C "${XDP_DIR}/lib/libbpf/src" && \
+    as_root make -j "${NPROC}" -C "${XDP_DIR}/lib/libbpf/src" install && \
+    DESTDIR="${PREFIX_DIR}" make -j "${NPROC}" -C "${XDP_DIR}/lib/libbpf/src" install && \
     popd && \
-    { ln -s /usr/local/include/xdp/xsk.h /usr/include/bpf/xsk.h || true; } && \
     return 0 || return 1
 }
 
 # Build and install the libfabrics
 function lib_install_fabrics()
 {
-    git_download_strip_unpack "ofiwg/libfabric" "${LIBFABRIC_VER}" "${LIBFABRIC_DIR}" && \
     pushd "${LIBFABRIC_DIR}" && \
     ./autogen.sh && \
     ./configure --enable-verbs && \
     make -j "${NPROC}" && \
-    make install && \
-    ldconfig && \
+    as_root make -j "${NPROC}" install && \
+    DESTDIR="${PREFIX_DIR}" make install && \
     popd && \
     return 0 || return 1
 }
 
-# Download the MTL and DPDK source code
-function lib_download_mtl_and_dpdk()
+# Build and install the DPDK
+function lib_install_dpdk()
 {
-    git_download_strip_unpack "${MTL_REPO}" "${MTL_VER}" "${MTL_DIR}" && \
-    git_download_strip_unpack "dpdk/dpdk" "refs/tags/v${DPDK_VER}" "${DPDK_DIR}" && \
-    return 0 || return 1
-}
-
-# Build and install the MTL and DPDK
-function lib_install_mtl_and_dpdk()
-{
-    # Patch and build the DPDK
     pushd "${DPDK_DIR}" && \
-    patch -p1 -i <(cat "${MTL_DIR}/patches/dpdk/${DPDK_VER}/"*.patch) && \
     meson setup build && \
-    ninja -C build && \
-    meson install -C build && \
+    ninja -j "${NPROC}" -C build && \
+    as_root meson install -C build && \
+    DESTDIR="${PREFIX_DIR}" as_root meson install -C build && \
     popd && \
-    # Build MTL
+    return 0 || return 1
+}
+
+# Build and install the MTL
+function lib_install_mtl()
+{
     pushd "${MTL_DIR}" && \
-    ./build.sh && \
-    meson install -C build && \
-    install script/nicctl.sh /usr/bin && \
-    ldconfig && \
+    as_root ./build.sh release && \
+    DESTDIR="${PREFIX_DIR}" as_root meson install -C build && \
+    as_root install script/nicctl.sh "${PREFIX_DIR:-}/usr/local/bin" && \
     popd && \
     return 0 || return 1
 }
@@ -262,21 +246,11 @@ function lib_install_mtl_and_dpdk()
 # Build and install gRPC from source
 function lib_install_grpc()
 {
-    rm -rf "${GRPC_DIR}"
-    mkdir -p "${GRPC_DIR}" && \
-    git clone --branch "${GPRC_VER}" \
-        --depth 1 \
-        --recurse-submodules \
-        --shallow-submodules \
-        https://github.com/grpc/grpc "${GRPC_DIR}" && \
     mkdir -p "${GRPC_DIR}/cmake/build" && \
     pushd "${GRPC_DIR}/cmake/build" && \
     cmake -DgRPC_BUILD_TESTS=OFF -DgRPC_INSTALL=ON ../.. && \
     make -j "${NPROC}" && \
-    make -j "${NPROC}" install && \
-    cmake -DgRPC_BUILD_TESTS=ON -DgRPC_INSTALL=ON ../.. && \
-    make -j "${NPROC}" grpc_cli && \
-    cp grpc_cli "/usr/local/bin/" && \
+    as_root make -j "${NPROC}" install && \
     popd && \
     return 0 || return 1
 }
@@ -284,11 +258,10 @@ function lib_install_grpc()
 # Build and install JPEG XS
 function lib_install_jpeg_xs()
 {
-    git_download_strip_unpack "OpenVisualCloud/SVT-JPEG-XS" "${JPEGXS_VER}" "${JPEGXS_DIR}" && \
-    mkdir -p "${JPEGXS_DIR}/Build/linux" "${JPEGXS_DIR}/imtl-plugin" && \
+    mkdir -p "${JPEGXS_DIR}/Build/linux" && \
     pushd "${JPEGXS_DIR}/Build/linux" && \
-    ./build.sh release install && \
-    ldconfig && \
+    as_root ./build.sh release install && \
+    as_root ./build.sh release --prefix "${PREFIX_DIR:-}/usr/local" install && \
     popd && \
     return 0 || return 1
 }
@@ -296,27 +269,42 @@ function lib_install_jpeg_xs()
 # Build and install JPEG XS imtl plugin
 function lib_install_mtl_jpeg_xs_plugin()
 {
+    mkdir -p "${JPEGXS_DIR}/imtl-plugin" "${PREFIX_DIR:-}/usr/local/etc/" && \
     pushd "${JPEGXS_DIR}/imtl-plugin" && \
     ./build.sh build-only && \
-    meson install --no-rebuild -C build && \
-    mkdir -p /usr/local/etc/ && \
-    cp -f "${JPEGXS_DIR}/imtl-plugin/kahawai.json" /usr/local/etc/imtl.json && \
+    as_root DESTDIR="${PREFIX_DIR}" meson install --no-rebuild -C build && \
+    as_root install "${JPEGXS_DIR}/imtl-plugin/kahawai.json" "${PREFIX_DIR:-}/usr/local/etc/imtl.json" && \
+    popd && \
+    return 0 || return 1
+}
+
+# Build and install MCM SDK
+function lib_build_install_sdk_mcm()
+{
+    pushd "${REPO_DIR}/sdk" && \
+    ./build.sh && \
     popd && \
     return 0 || return 1
 }
 
 function full_build_and_install_workflow()
 {
+    log_info Starting: Dependencies build, install and configation.
     lib_install_grpc && \
     lib_install_xdp_bpf_tools && \
     lib_install_fabrics && \
-    lib_install_mtl_and_dpdk && \
+    lib_install_dpdk && \
+    lib_install_mtl && \
     lib_install_jpeg_xs && \
-    lib_install_mtl_jpeg_xs_plugin && \
-    chmod -R a+r "${BUILD_DIR}" && \
-    return 0 || return 1
+    lib_install_mtl_jpeg_xs_plugin
+    return_code="$?"
+    if [[ "${return_code}" != "0" ]]; then
+        log_error "Dependencies build, install and configation failed."
+    else
+        log_success "Finished: Dependencies build, install and configation."
+    fi
+    return "${return_code}"
 }
-
 # cp -f "${REPO_DIR}/media-proxy/imtl.json" "/usr/local/etc/imtl.json"
 # export KAHAWAI_CFG_PATH="/usr/local/etc/imtl.json"
 
@@ -324,34 +312,18 @@ function full_build_and_install_workflow()
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
     if [ "${EUID}" != "0" ]; then
-        error "Must be run as root. Try running bellow command:"
-        error "sudo \"${BASH_SOURCE[0]}\""
-        exit 1
+        log_error "Must be run as root. Try running below command:"
+        log_error "sudo \"${BASH_SOURCE[0]}\""
+        exit 3
     fi
-    print_logo_anim "2" "0.04"
-    sleep 1
+    if [ "${NO_LOGO_PRINT:-0}" != "0" ]; then
+        print_logo_anim "2" "0.04"
+        sleep 2
+    fi
     trap_error_print_debug
-    sleep 1
-    prompt Starting: OS packages installation, MTL and DPDK download.
     install_package_dependencies
-    lib_download_mtl_and_dpdk
-    prompt Finished: OS packages installation, MTL and DPDK download.
-    prompt Starting: Intel drivers download and patch apply.
-    get_and_patch_intel_drivers
-    prompt Finished: Intel drivers download and patch apply.
-    prompt Starting: Dependencies build, install and configation.
-    full_build_and_install_workflow || \
-    { error Dependencies build, install and configation failed. && exit 1; }
-    prompt Finished: Dependencies build, install and configation.
-    prompt Starting: Build, install and configuration of Intel drivers.
-    build_install_and_config_intel_drivers || \
-    { error Intel drivers configuration/installation failed. && exit 1; }
-    build_install_and_config_irdma_drivers || \
-    { error Intel irdma configuration/installation failed. && exit 1; }
-    prompt Finished: Build, install and configuration of Intel drivers.
-    prompt All tasks compleated. Reboot required.
-    warning ""
-    warning OS reboot is required for all of the changes to take place.
-    sleep 2
+    get_download_unpack_dependencies
+    full_build_and_install_workflow
+    log_success "All tasks compleated successfuly. Happy MCM'ing ;-)"
     exit 0
 fi
