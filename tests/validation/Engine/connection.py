74c11508ae7ec2fe909f65a2d9e1bcd76e843477
MateuszGrabuszynski <mateusz.grabuszynski@intel.com>
2024-12-11 15:51:11 +0000

    Adding more enums, fixing issues with missing : and self

--- tests/validation/Engine/connection.py
+++ tests/validation/Engine/connection.py
@@ -9,25 +9,66 @@ class ConnectionType(Enum):
 class Connection:
     """Class used to prepare connection part of connection.json config file"""
     connection_type = None
-    def __init__(self, connection_type=ConnectionType.MPG)
+    def __init__(self, connection_type=ConnectionType.MPG):
         self.connection_type = connection_type
+
+    def toDict(self) -> dict:
+        if self.connection_type == ConnectionType.MPG:
+            return {
+                "connection": {
+                    "multipoint-group": {
+                        "urn": self.urn
+                    }
+                }
+            }
+        elif self.connection_type == ConnectionType.ST2110:
+            return {
+                "connection": {
+                    "st2110": {
+                        "transport": self.transport.value, #FIXME: a tuple with one value?
+                        "remoteIpAddr": self.remoteIpAddr,
+                        "remotePort": self.remotePort,
+                        "pacing": self.pacing,
+                        "payloadType": self.payloadType
+                    }
+                }
+            }
+        elif self.connection_type == ConnectionType.RDMA:
+            return {
+                "connection": {
+                    "rdma": {
+                        "connectionMode": self.connectionMode.value,
+                        "maxLatencyNs": self.maxLatencyNs
+                    }
+                }
+            }
 
 
 class MultipointGroup(Connection):
     """Prepares multipoint-group part of connection.json file"""
     def __init__(
+        self,
         urn="ipv4:224.0.0.1:9003"
     ):
         self.urn = urn
+        self.connection_type = ConnectionType.MPG
 
     def set_multipointgroup(edits: dict):
         self.urn = edits.get("urn", self.urn)
 
 
+class TransportType(Enum):
+    ST20 = "st2110-20",
+    ST22 = "st2110-22",
+    ST30 = "st2110-30",
+    ST40 = "st2110-40",
+    ST41 = "st2110-41"
+
 class St2110(Connection):
     """Prepares st2110 part of connection.json file"""
     def __init__(
-        transport="st2110-20",
+        self,
+        transport=TransportType.ST20,
         remoteIpAddr="192.168.95.2",
         remotePort="9002",
         pacing="narrow",
@@ -38,6 +79,7 @@ class St2110(Connection):
         self.remotePort = remotePort
         self.pacing = pacing
         self.payloadType = payloadType
+        self.connection_type = ConnectionType.ST2110
 
     def set_st2110(edits: dict):
         self.transport = edits.get("transport", self.transport)
@@ -46,15 +88,22 @@ class St2110(Connection):
         self.pacing = edits.get("pacing", self.pacing)
         self.payloadType = edits.get("payloadType", self.payloadType)
 
+class ConnectionMode(Enum):
+    RC = "RC"
+    UC = "UC"
+    UD = "UD"
+    RD = "RD"
 
 class Rdma(Connection):
     """Prepares RDMA part of connection.json file"""
     def __init__(
-        connectionMode="RC",
+        self,
+        connectionMode=ConnectionMode.RC,
         maxLatencyNs=10000
     ):
         self.connectionMode = connectionMode
         self.maxLatencyNs = maxLatencyNs
+        self.connection_type = ConnectionType.RDMA
 
     def set_rdma(edits: dict):
         self.connectionMode = edits.get("connectionMode", self.connectionMode)
--- tests/validation/Engine/connection_json.py
+++ tests/validation/Engine/connection_json.py
@@ -1,11 +1,18 @@
+from connection import Connection
+from payload import Payload
+
 class ConnectionJson:
     """Class used to prepare connection.json config file"""
-
     def __init__(
+        self,
         bufferQueueCapacity=16,
         maxPayloadSize=2097152,
         maxMetadataSize=8192,
+        connection=Connection(),
+        payload=Payload()
     )
         self.bufferQueueCapacity = bufferQueueCapacity
         self.maxPayloadSize = maxPayloadSize
         self.maxMetadataSize = maxMetadataSize
+        self.connection = connection
+        self.payload = payload
