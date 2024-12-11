362ee18f696d6fb6143ae05d6db8443bef46e9f0
MateuszGrabuszynski <mateusz.grabuszynski@intel.com>
2024-12-11 15:51:54 +0000

    Creating connection_usage_example.py and adding a missing newline

---
+++ tests/validation/Engine/connection_usage_example.py
@@ -0,0 +1,34 @@
+from connection import ConnectionMode, Rdma, TransportType, St2110
+
+connection = Rdma(
+    connectionMode=ConnectionMode.UC,
+    maxLatencyNs=30000
+)
+
+print(f"""connection info:
+connectionMode={connection.connectionMode}
+maxLatencyNs={connection.maxLatencyNs}
+""")
+
+
+print(f"""Dict:
+{connection.toDict()}
+""")
+
+conn = St2110(
+    transport=TransportType.ST30,
+    pacing="wide"
+)
+
+print(f"""conn info:
+transport={conn.transport}
+remoteIpAddr={conn.remoteIpAddr}
+remotePort={conn.remotePort}
+pacing={conn.pacing}
+payloadType={conn.payloadType}
+""")
+
+
+print(f"""Dict:
+{conn.toDict()}
+""")
