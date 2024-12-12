5e5cac5571a466dec88de099a8d5932d7df8b30d
MateuszGrabuszynski <mateusz.grabuszynski@intel.com>
2024-12-12 12:41:26 +0000

    Finalized ConnectionJson object and added an example

--- tests/validation/Engine/connection_json.py
+++ tests/validation/Engine/connection_json.py
@@ -1,3 +1,5 @@
+import json
+
 from connection import Connection
 from payload import Payload
 
@@ -10,9 +12,21 @@ class ConnectionJson:
         maxMetadataSize=8192,
         connection=Connection(),
         payload=Payload()
-    )
+    ):
         self.bufferQueueCapacity = bufferQueueCapacity
         self.maxPayloadSize = maxPayloadSize
         self.maxMetadataSize = maxMetadataSize
         self.connection = connection
         self.payload = payload
+
+    def toDict(self) -> dict:
+        return {
+            "bufferQueueCapacity": self.bufferQueueCapacity,
+            "maxPayloadSize": self.maxPayloadSize,
+            "maxMetadataSize": self.maxMetadataSize,
+            **self.connection.toDict(),
+            **self.payload.toDict()
+        }
+
+    def toJson(self):
+        return json.dumps(self.toDict())
