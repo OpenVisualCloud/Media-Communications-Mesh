dc48155c50caa554529b68a5770e3fcafa159946
MateuszGrabuszynski <mateusz.grabuszynski@intel.com>
2024-12-18 17:52:14 +0100

    Trying to add a fixture to kill existing media_proxies and spawn new ones

--- tests/validation/Engine/fixtures_mcm.py
+++ tests/validation/Engine/fixtures_mcm.py
@@ -16,10 +16,34 @@ def build_TestApp(build):
     subprocess.run("make", cwd=path, shell=True, timeout=10)
 
 
-@pytest.fixture(scope="package")
-def media_proxy_single():
-    # Run single media proxy
-    pass
+def kill_all_existing_media_proxies():
+    "Kills all existing media_proxy processes using their PIDs found with px -aux"
+    existing_mps = subprocess.run("ps -aux | awk '/media_proxy/{print($2)}'", shell=True, capture_output=True)
+    mp_pids = existing_mps.stdout.decode("utf-8").split()  # returns an array of PIDs
+    (subprocess.run(f"kill -9 {mp_pid}", shell=True) for mp_pid in mp_pids)
+
+
+@pytest.fixture(scope="package", autouse=False)
+def media_proxy_single(sender_mp_port: int, receiver_mp_port: int, kill_existing: bool = True):
+    """Opens new media_proxies for sender and receiver.
+    May optionally kill the already-running media_proxies first.
+
+    Arguments:
+        sender_mp_port(int): specifies the port number for sender
+        receiver_mp_port(int): specifies the port number for receiver
+
+    Keyword arguments:
+        kill_existing(bool, optional): if use kill_all_existing_media_proxies() function to kill -9 all existing
+                                       media_proxies before running new instances
+    """
+    if kill_existing:
+        kill_all_existing_media_proxies()
+    # create new media_proxy processes for sender and receiver
+    sender_mp_proc = subprocess.Popen(f"media_proxy -t {sender_mp_port}")  # sender's media_proxy
+    receiver_mp_proc = subprocess.Popen(f"media_proxy -t {receiver_mp_port}")  # receiver media_proxy
+    yield
+    sender_mp_proc.terminate()
+    receiver_mp_proc.terminate()
 
 
 @pytest.fixture(scope="package")
