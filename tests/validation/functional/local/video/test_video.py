439d2a8735d2c7aee8e104f3e4a3e9da5b08e222
MateuszGrabuszynski <mateusz.grabuszynski@intel.com>
2024-12-19 12:21:52 +0000

    Revert "Properly marking test_video.py with fixtures"
    This reverts commit a6b8945711fc83e4c65f2d04556ea396c5a547fd.

--- tests/validation/functional/local/video/test_video.py
+++ tests/validation/functional/local/video/test_video.py
@@ -2,7 +2,6 @@
 # Copyright 2024 Intel Corporation
 # Intel® Media Communications Mesh
 import os
-import pytest
 
 import Engine.client_json
 import Engine.connection
@@ -11,12 +10,9 @@ import Engine.engine_mcm as utils
 import Engine.execute
 import Engine.payload
 from Engine.media_files import yuv_files
-from Engine.fixtures_mcm import media_proxy_single, build_TestApp
 
 
-@pytest.mark.fixture(media_proxy_single)
-@pytest.mark.fixture(build_TestApp)
-def test_video(build: str, media: str):
+def test_video(build_TestApp, build: str, media: str):
     client = Engine.client_json.ClientJson()
     conn_mpg = Engine.connection.MultipointGroup()
     payload = Engine.payload.Video(width=3840, height=2160)
