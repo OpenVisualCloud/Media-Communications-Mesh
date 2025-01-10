012268d077fcf7197659a48844aae79d50176702
MateuszGrabuszynski <mateusz.grabuszynski@intel.com>
2025-01-10 14:21:41 +0000

    Add: tests of mock for cluster and st2110 video transfers

---
+++ tests/validation/functional/st2110/st20/test_video.py
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2024 Intel Corporation
+# Intel® Media Communications Mesh
+import os
+import pytest
+
+import Engine.client_json
+import Engine.connection
+import Engine.connection_json
+import Engine.engine_mcm as utils
+import Engine.execute
+import Engine.payload
+from Engine.media_files import yuv_files
+
+
+@pytest.mark.parametrize("video_type", [k for k in yuv_files.keys()])
+def test_video(build_TestApp, build: str, media: str, video_type):
+    client = Engine.client_json.ClientJson()
+    conn_mpg = Engine.connection.St2110()
+    payload = Engine.payload.Video(
+        width=yuv_files[video_type]["width"],
+        height=yuv_files[video_type]["height"],
+        fps=yuv_files[video_type]["fps"],
+        pixelFormat=yuv_files[video_type]["format"],
+    )
+    connection = Engine.connection_json.ConnectionJson(connection=conn_mpg, payload=payload)
+
+    utils.create_client_json(build, client)
+    utils.create_connection_json(build, connection)
+
+    # Use a specified file from media_files.py
+    media_file = yuv_files[video_type]["filename"]
+    media_file_path = os.path.join(media, media_file)
+
+    utils.run_rx_tx_with_file(file_path=media_file_path, build=build)
