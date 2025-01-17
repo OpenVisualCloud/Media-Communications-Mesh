05492f81403b00bc521ef935567ca3efa31c16cd
Miłosz Linkiewicz <milosz.linkiewicz@intel.com>
2025-01-17 13:05:53 +0100

    Update and fix the coverity.yml (#310)
    FIX: Update the coverity scan
    - Update Coverity.yml by adding 3rd party library workaround.
    - Update Coverity.yml to build without unit tests as this is not a point of interest.
    
    - Minor GitHub actions versions and flags update
    - Updated scan-dependency-review
    - Fixes to Super-Linter regarding speed and configuration symbolic links
    
    ---------
    
    Signed-off-by: Milosz Linkiewicz <milosz.linkiewicz@intel.com>

--- scripts/setup_build_env.sh
+++ scripts/setup_build_env.sh
@@ -197,7 +197,8 @@ function lib_build_and_install_jsonc()
 # Get and install golang from source
 function lib_build_and_install_golang()
 {
-    as_root wget_download_strip_unpack "https://go.dev/dl/go${GOLANG_GO_VER}.linux-amd64.tar.gz" "/usr/local/go/"
+    wget_download_strip_unpack "https://go.dev/dl/go${GOLANG_GO_VER}.linux-amd64.tar.gz" "${BUILD_DIR}/golang"
+    as_root cp -r "${BUILD_DIR}/golang" "/usr/local/go"
     as_root ln -s /usr/local/go/bin/go /usr/bin/go
     go version
     go install "${GOLANG_PROTOBUF_GEN}"
