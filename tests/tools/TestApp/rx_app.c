f758736a7d09c1def32df742f6075523850c1173
MateuszGrabuszynski <mateusz.grabuszynski@intel.com>
2025-01-23 13:13:25 +0000

    Changing usage message for path to file

--- tests/tools/TestApp/rx_app.c
+++ tests/tools/TestApp/rx_app.c
@@ -13,9 +13,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
     if (argc != 4) {
-        fprintf(stderr,
-                "Usage: %s <client_cfg.json> <connection_cfg.json> <abs path>/file|frame>\n",
-                argv[0]);
+        fprintf(stderr, "Usage: %s <client_cfg.json> <connection_cfg.json> <path_to_output_file>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
