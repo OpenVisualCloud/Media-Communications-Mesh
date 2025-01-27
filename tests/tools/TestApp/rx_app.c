3ba872afe05de4796b39a5230f06f584f387f233
Lukas G <20104521+zLukas@users.noreply.github.com>
2025-01-27 19:43:10 +0100

    Val: Un-mocking the TestApp with actual API-based implementation (#308)
    * sdk succesfully linked to TestApp
    
    
    Signed-off-by: Lukas G <20104521+zLukas@users.noreply.github.com>
    Signed-off-by: Mateusz Grabuszyński <mgrabuszynski@gmail.com>

--- tests/tools/TestApp/rx_app.c
+++ tests/tools/TestApp/rx_app.c
@@ -1,26 +1,59 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "Inc/input.h"
 #include "Inc/mcm.h"
-#include <unistd.h>
 
-const char* client_cfg;
-const char* conn_cfg;
+char *client_cfg;
+char *conn_cfg;
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
+    if (!is_root()) {
+        fprintf(stderr, "This program must be run as root. Exiting.\n");
+        exit(EXIT_FAILURE);
+    }
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <client_cfg.json> <connection_cfg.json> <path_to_output_file>\n",
+                argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-  printf("launching RX App \n");
-  printf("RX App PID: %d\n", getpid());
-  printf("reading client configuration... \n");  
-  client_cfg = parse_json_to_string("client.json");
-  printf("reading connection configuration... \n");
-  conn_cfg = parse_json_to_string("connection.json");
-  mcm_ts mcm;
-  mcm_init_client(&mcm, client_cfg);
-  mcm_create_rx_connection(&mcm, conn_cfg);
-  printf("waiting for frames... \n");
-  while(1){
-    // mcm_create_rx_connection(&mcm, conn_cfg);
-    // mcm_receive_video_frames(&mcm);
-  }
-  return 0;
+    char *client_cfg_file = argv[1];
+    char *conn_cfg_file = argv[2];
+    char *out_filename = argv[3];
+
+    MeshConnection *connection = NULL;
+    MeshClient *client = NULL;
+
+    printf("[RX] Launching RX App \n");
+    printf("[RX] Reading client configuration... \n");
+    client_cfg = parse_json_to_string(client_cfg_file);
+    printf("[RX] Reading connection configuration... \n");
+    conn_cfg = parse_json_to_string(conn_cfg_file);
+
+    /* Initialize mcm client */
+    int err = mesh_create_client_json(&client, client_cfg);
+    if (err) {
+        printf("[RX] Failed to create mesh client: %s (%d)\n", mesh_err2str(err), err);
+        goto safe_exit;
+    }
+
+    /* Create mesh connection */
+    err = mesh_create_rx_connection(client, &connection, conn_cfg);
+    if (err) {
+        printf("[RX] Failed to create connection: %s (%d)\n", mesh_err2str(err), err);
+        mesh_delete_client(&client);
+        goto safe_exit;
+    }
+    printf("[RX] Waiting for frames... \n");
+    read_data_in_loop(connection, out_filename);
+    printf("[RX] Shuting down connection and client\n");
+    mesh_delete_connection(&connection);
+    mesh_delete_client(&client);
+    printf("[RX] Shutdown completed exiting\n");
+
+safe_exit:
+    free(client_cfg);
+    free(conn_cfg);
+    return err;
 }
