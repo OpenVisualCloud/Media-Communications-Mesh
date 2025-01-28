8935e44d131b225d7f6ba76099dd4af5c1b938ba
Mateusz Grabuszyński <mgrabuszynski@gmail.com>
2025-01-28 17:31:52 +0100

    Val: video test stubs and media-proxy related fixtures (#304)
    * Add: stub for fixtures relating to media_proxy process lifecycle (spawning, killing)
    * Add: removal of sent file at the end of test to avoid disk full errors
    * Add: tests of mock for cluster and st2110 video transfers
    * Fix: wrong connection.st2110.transport value
    * Add: todos for further code extension
    * in tests/validation: Extending copyright with 2024-2025
    * in tests/validation: Removing 'Intel(R)' from Media Communications Mesh

--- tests/validation/Engine/fixtures_mcm.py
+++ tests/validation/Engine/fixtures_mcm.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2024 Intel Corporation
-# Intel® Media Communications Mesh
+# Copyright 2024-2025 Intel Corporation
+# Media Communications Mesh
 import os
 import subprocess
 
@@ -10,16 +10,42 @@ import pytest
 @pytest.fixture(scope="session")
 def build_TestApp(build):
     path = os.path.join(build, "tests", "tools", "TestApp", "build")
-    subprocess.run(f"rm -rf {path}", shell=True, timeout=10)
-    subprocess.run(f"mkdir -p {path}", shell=True, timeout=10)
+    subprocess.run(f'rm -rf "{path}"', shell=True, timeout=10)
+    subprocess.run(f'mkdir -p "{path}"', shell=True, timeout=10)
     subprocess.run("cmake ..", cwd=path, shell=True, timeout=10)
     subprocess.run("make", cwd=path, shell=True, timeout=10)
 
 
-@pytest.fixture(scope="package")
-def media_proxy_single():
-    # Run single media proxy
-    pass
+def kill_all_existing_media_proxies():
+    # TODO: This assumes the way previous media_proxy worked will not change in the new version, which is unlikely
+    "Kills all existing media_proxy processes using their PIDs found with px -aux"
+    existing_mps = subprocess.run("ps -aux | awk '/media_proxy/{print($2)}'", shell=True, capture_output=True)
+    mp_pids = existing_mps.stdout.decode("utf-8").split()  # returns an array of PIDs
+    (subprocess.run(f"kill -9 {mp_pid}", shell=True) for mp_pid in mp_pids)
+
+
+@pytest.fixture(scope="package", autouse=False)
+def media_proxy_single(sender_mp_port: int, receiver_mp_port: int, kill_existing: bool = True):
+    # TODO: This assumes the way previous media_proxy worked will not change in the new version, which is unlikely
+    """Opens new media_proxies for sender and receiver.
+    May optionally kill the already-running media_proxies first.
+
+    Arguments:
+        sender_mp_port(int): specifies the port number for sender
+        receiver_mp_port(int): specifies the port number for receiver
+
+    Keyword arguments:
+        kill_existing(bool, optional): if use kill_all_existing_media_proxies() function to kill -9 all existing
+                                       media_proxies before running new instances
+    """
+    if kill_existing:
+        kill_all_existing_media_proxies()
+    # create new media_proxy processes for sender and receiver
+    sender_mp_proc = subprocess.Popen(f"media_proxy -t {sender_mp_port}")  # sender's media_proxy
+    receiver_mp_proc = subprocess.Popen(f"media_proxy -t {receiver_mp_port}")  # receiver media_proxy
+    yield
+    sender_mp_proc.terminate()
+    receiver_mp_proc.terminate()
 
 
 @pytest.fixture(scope="package")
