4641f70672bbb51a67d68d8175a13e8f7eb1ab1f
MateuszGrabuszynski <mateusz.grabuszynski@intel.com>
2025-01-29 17:22:19 +0000

    add: integrity checks - number of frames, chunk size, more verbose output, more format options in calculate_yuv_frame_size

--- tests/validation/Engine/integrity.py
+++ tests/validation/Engine/integrity.py
@@ -2,17 +2,20 @@
 # Copyright(c) 2024-2025 Intel Corporation
 
 import hashlib
+import logging
 import re
 from math import floor
 
 from Engine.execute import log_fail
 
 
-def check_st20p_integrity(src_url: str, out_url: str, frame_size: int):
+def check_st20p_integrity(src_url: str, out_url: str, frame_size: int, expected_frame_percentage:int = 80):
     src_chunk_sums = []
 
     with open(src_url, "rb") as f:
         while chunk := f.read(frame_size):
+            if len(chunk) != frame_size:
+                logging.debug(f"SOURCE CHUNK SIZE MISMATCH {len(chunk)} != {frame_size}")
             chunk_sum = hashlib.md5(chunk).hexdigest()
             src_chunk_sums.append(chunk_sum)
 
@@ -20,28 +23,40 @@ def check_st20p_integrity(src_url: str, out_url: str, frame_size: int):
 
     with open(out_url, "rb") as f:
         while chunk := f.read(frame_size):
+            if len(chunk) != frame_size:
+                logging.debug(f"OUTPUT CHUNK SIZE MISMATCH {len(chunk)} != {frame_size}")
             chunk_sum = hashlib.md5(chunk).hexdigest()
             out_chunk_sums.append(chunk_sum)
+
+    logging.debug("SOURCE CHUNKS:")
+    logging.debug(src_chunk_sums)
+    logging.debug("OUTPUT CHUNKS:")
+    logging.debug(out_chunk_sums)
 
     if len(src_chunk_sums) < len(out_chunk_sums):
         for i in range(len(src_chunk_sums)):
             if src_chunk_sums[i] != out_chunk_sums[i]:
-                log_fail(f"Received frame {i} is invalid")
+                logging.debug(f"Received frame {i+1} is invalid")
                 return False
     else:
         for i in range(len(out_chunk_sums)):
             if out_chunk_sums[i] != src_chunk_sums[i]:
-                log_fail(f"Received frame {i} is invalid")
+                logging.debug(f"Received frame {i+1} is invalid")
                 return False
+    # Ensure enough frames were sent
+    if len(out_chunk_sums) / len(src_chunk_sums) * 100.00 < expected_frame_percentage:
+        logging.warning(f"Received only {len(out_chunk_sums)/len(src_chunk_sums) * 100:.2f}% of the frames! Expected: {expected_frame_percentage:.2f}%")
+    else:
+        logging.debug(f"Received {len(out_chunk_sums)/len(src_chunk_sums) * 100:.2f}% of the frames")
 
     return True
 
 
 def calculate_yuv_frame_size(width: int, height: int, file_format: str):
     match file_format:
-        case "YUV422RFC4175PG2BE10":
+        case "YUV422RFC4175PG2BE10" | "yuv422p10rfc4175":
             pixel_size = 2.5
-        case "YUV422PLANAR10LE":
+        case "YUV422PLANAR10LE" | "yuv422p10le":
             pixel_size = 4
         case _:
             log_fail(f"Size of {file_format} pixel is not known")
@@ -62,15 +77,20 @@ def check_st30p_integrity(src_url: str, out_url: str, size: int):
         while chunk := f.read(size):
             out_chunks.append(chunk)
 
+    logging.debug("SOURCE CHUNKS:")
+    logging.debug(src_chunks)
+    logging.debug("OUTPUT CHUNKS:")
+    logging.debug(out_chunks)
+
     if len(src_chunks) < len(out_chunks):
         for i in range(len(src_chunks)):
             if src_chunks[i] != out_chunks[i]:
-                log_fail(f"Received frame {i} is invalid")
+                logging.debug(f"Received frame {i+1} is invalid")
                 return False
     else:
         for i in range(len(out_chunks)):
             if out_chunks[i] != src_chunks[i]:
-                log_fail(f"Received frame {i} is invalid")
+                logging.debug(f"Received frame {i+1} is invalid")
                 return False
 
     return True
