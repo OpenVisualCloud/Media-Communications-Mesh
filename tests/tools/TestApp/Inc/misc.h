7653658ec7db6ac987296466f766e267feb66cc3
Konstantin Ilichev <konstantin.ilichev@intel.com>
2025-01-30 19:20:18 +0300

    Add timestamp printing in log messages in tests (#323)
    Usage:
    LOG("Message with format %d", 123);
    
    Output:
    Jan 30 14:20:02.534  Message with format 123
    
    Signed-off-by: Konstantin Ilichev <konstantin.ilichev@intel.com>

---
+++ tests/tools/TestApp/Inc/misc.h
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Intel Corporation
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _MISC_H_
+#define _MISC_H_
+
+void LOG(const char *format, ...);
+
+#endif /* _MISC_H_ */
