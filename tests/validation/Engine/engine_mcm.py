1bcf064a7ac7c3ed6c75a099898565faf7e86023
Mateusz Grabuszyński <mgrabuszynski@gmail.com>
2025-02-04 12:25:56 +0100

    Val: Switching mock app to Łappka in pytest (#322)
    Switches mock SDK testing application to Łappka (application using real Media Communications Mesh SDK API) with the most basic tests for now.
    
    Fix: mcm.c switch from unsigned char (0-255) to unsigned int as a frame counter
    Fix: multipoint-group -> multipointGroup in JSON generator
    Fix: localhost address and default port ensure stable connection for a single media_proxy test
    Add: mesh-agent starts before media_proxy
    Add: fixed sleep-based timing for application runtime (ensures stability)
    Add: more logging
    Add: integrity tests from MTL (from @staszczuk)
    Add: additional file removal function for pathlib-based paths (with backwards compatibility)
    Fix: calculate_yuv_frame_size() uses proper values
    Add: file_format in media_files.py
    Add: logging the output of each executed component
    Mod: cleanup of integrity.py

--- tests/validation/Engine/engine_mcm.py
+++ tests/validation/Engine/engine_mcm.py
@@ -5,6 +5,7 @@
 import logging
 import signal
 import subprocess
+import time
 
 from pathlib import Path
 
@@ -13,9 +14,20 @@ import Engine.connection
 import Engine.connection_json
 import Engine.execute
 import Engine.payload
+from Engine.integrity import calculate_yuv_frame_size, check_st20p_integrity
 
 
-def create_client_json(build: str, client: Engine.client_json.ClientJson):
+video_format_matches = {
+    # file_format : payload format
+    "YUV422PLANAR10LE": "yuv422p10le",
+    "YUV422RFC4175PG2BE10": "yuv422p10rfc4175",
+}
+
+def video_file_format_to_payload_format(pixel_format: str) -> str:
+    return video_format_matches.get(pixel_format, pixel_format) # matched if matches, else original
+
+
+def create_client_json(build: str, client: Engine.client_json.ClientJson) -> None:
     logging.debug("Client JSON:")
     for line in client.to_json().splitlines():
         logging.debug(line)
@@ -24,7 +36,7 @@ def create_client_json(build: str, client: Engine.client_json.ClientJson):
     client.prepare_and_save_json(output_path=output_path)
 
 
-def create_connection_json(build: str, connection: Engine.connection_json.ConnectionJson):
+def create_connection_json(build: str, connection: Engine.connection_json.ConnectionJson) -> None:
     logging.debug("Connection JSON:")
     for line in connection.to_json().splitlines():
         logging.debug(line)
@@ -33,21 +45,21 @@ def create_connection_json(build: str, connection: Engine.connection_json.Connec
     connection.prepare_and_save_json(output_path=output_path)
 
 
-def run_rx_app(cwd: str, timeout: int = 0) -> Engine.execute.AsyncProcess:
-    return Engine.execute.call("./RxApp", cwd=cwd, timeout=timeout)
+def run_rx_app(client_cfg_file: str, connection_cfg_file: str, path_to_output_file: str, cwd: str, timeout: int = 0) -> Engine.execute.AsyncProcess:
+    return Engine.execute.call(f"./RxApp {client_cfg_file} {connection_cfg_file} {path_to_output_file}", cwd=cwd, timeout=timeout)
 
 
-def run_tx_app(file_path: str, rx_pid: int, cwd: str, testcmd: bool = True) -> subprocess.CompletedProcess:
-    return Engine.execute.run(f"./TxApp {file_path} {rx_pid}", testcmd=testcmd, cwd=cwd)
+def run_tx_app(client_cfg_file: str, connection_cfg_file: str, path_to_input_file: str, cwd: str, testcmd: bool = True) -> subprocess.CompletedProcess:
+    return Engine.execute.run(f"./TxApp {client_cfg_file} {connection_cfg_file} {path_to_input_file}", cwd=cwd)
 
 
-def handle_tx_failure(tx: subprocess.CompletedProcess):
+def handle_tx_failure(tx: subprocess.CompletedProcess) -> None:
     if tx.returncode != 0:
         Engine.execute.log_fail(f"TxApp failed with return code {tx.returncode}")
 
 
-def stop_rx_app(rx: Engine.execute.AsyncProcess):
-    rx.process.send_signal(signal.SIGINT)
+def stop_rx_app(rx: Engine.execute.AsyncProcess) -> None:
+    rx.process.terminate()
     rx.process.wait()
 
 
@@ -63,14 +75,44 @@ def remove_sent_file(file_path: str, app_path: str) -> None:
         logging.debug(f"Cannot remove. File does not exist: {removal_path}")
 
 
-def run_rx_tx_with_file(file_path: str, build: str):
+def remove_sent_file(full_path: Path) -> None:
+    try:
+        full_path.unlink()
+        logging.debug(f"Removed: {full_path}")
+    # except makes the test pass if there's no file to remove
+    except (FileNotFoundError, NotADirectoryError):
+        logging.debug(f"Cannot remove. File does not exist: {full_path}")
+
+
+def run_rx_tx_with_file(file_path: str, build: str, timeout: int = 0, media_info = {}) -> None:
     app_path = Path(build, "tests", "tools", "TestApp", "build")
 
     try:
-        rx = run_rx_app(cwd=app_path)
-        tx = run_tx_app(file_path=file_path, rx_pid=rx.process.pid, cwd=app_path)
+        client_cfg_file = Path(app_path.resolve(), "client.json")
+        connection_cfg_file = Path(app_path.resolve(), "connection.json")
+        output_file_path = Path(app_path, Path(file_path).name + "_MCMoutput.yuv").resolve()
+        rx = run_rx_app(
+            client_cfg_file=client_cfg_file,
+            connection_cfg_file=connection_cfg_file,
+            path_to_output_file=str(output_file_path),
+            cwd=app_path,
+            timeout=timeout
+            )
+        time.sleep(2) # 2 seconds for RxApp to spin up
+        tx = run_tx_app(
+            client_cfg_file=client_cfg_file,
+            connection_cfg_file=connection_cfg_file,
+            path_to_input_file=file_path,
+            cwd=app_path
+            )
+        time.sleep(5) # 5 seconds for TxApp to shut down before handling it as a failure
         handle_tx_failure(tx)
-    finally:
         stop_rx_app(rx)
-        # TODO: Add checks for transmission errors
-        remove_sent_file(file_path, app_path)
+    finally:
+        frame_size = calculate_yuv_frame_size(media_info.get("width"), media_info.get("height"), media_info.get("pixelFormat"))
+        integrity_check = check_st20p_integrity(file_path, str(output_file_path), frame_size)
+        logging.debug(f"Integrity: {integrity_check}")
+        remove_sent_file(output_file_path)
+
+        if not integrity_check:
+            Engine.execute.log_fail("At least one of the received frames has not passed the integrity test")
