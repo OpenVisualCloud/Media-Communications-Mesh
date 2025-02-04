28399e672c27b91cb7ac39528d24ed5b8ffed7df
MateuszGrabuszynski <mateusz.grabuszynski@intel.com>
2025-02-04 21:10:46 +0000

    (not working) MPG workaround for RDMA connection
    Add: modifiable json filenames in create_*_json functions
    Fix: env variable MCM_MEDIA_PROXY_PORT takes string as dict value instead of int
    Add: run_rx_tx_with_file() arguments for variable json filenames
    Rem: TODO msg from kill_all_existing_media_proxies() as unnecessary
    Add: media_proxy_cluster rdma parameters (ip, port)
    Add: cluster test_video split rx and tx client json files
    Workaround SDBQ-1949: cluster test_video uses MultipointGroup with urn to achieve RDMA connections

--- tests/validation/Engine/fixtures_mcm.py
+++ tests/validation/Engine/fixtures_mcm.py
@@ -21,7 +21,6 @@ def build_TestApp(build: str) -> None:
 
 
 def kill_all_existing_media_proxies() -> None:
-    # TODO: This assumes the way previous media_proxy worked will not change in the new version, which is unlikely
     "Kills all existing media_proxy processes using their PIDs found with px -aux"
     existing_mps = subprocess.run("ps -aux | awk '/media_proxy/{print($2)}'", shell=True, capture_output=True)
     mp_pids = existing_mps.stdout.decode("utf-8").split()  # returns an array of PIDs
@@ -72,7 +71,14 @@ def media_proxy_single() -> None:
 
 # Run dual media proxy
 @pytest.fixture(scope="package", autouse=False)
-def media_proxy_cluster(tx_mp_port: int = 8002, rx_mp_port: int = 8003) -> None:
+def media_proxy_cluster(
+    tx_mp_port: int = 8002,
+    rx_mp_port: int = 8003,
+    tx_rdma_ip: str = "192.168.95.1",
+    rx_rdma_ip: str = "192.168.95.2",
+    tx_rdma_port_range: str = "9100-9119",
+    rx_rdma_port_range: str = "9120-9139",
+    ) -> None:
     # kill all existing media proxies first
     kill_existing = True # TODO: Make it parametrized
     if kill_existing:
@@ -85,13 +91,13 @@ def media_proxy_cluster(tx_mp_port: int = 8002, rx_mp_port: int = 8003) -> None:
         logging.debug(f"mesh-agent return code: {mesh_agent_proc.returncode} of type {type(mesh_agent_proc.returncode)}")
 
     # start sender media_proxy
-    sender_mp_proc = Engine.execute.call(f"media_proxy -t {tx_mp_port}", cwd=".")
+    sender_mp_proc = Engine.execute.call(f"media_proxy -t {tx_mp_port} -r {tx_rdma_ip} -p {tx_rdma_port_range}", cwd=".")
     time.sleep(0.2) # short sleep used for media_proxy to spin up
     if sender_mp_proc.process.returncode:
         logging.debug(f"sender media_proxy return code: {sender_mp_proc.returncode} of type {type(sender_mp_proc.returncode)}")
 
     # start receiver media_proxy
-    receiver_mp_proc = Engine.execute.call(f"media_proxy -t {rx_mp_port}", cwd=".")
+    receiver_mp_proc = Engine.execute.call(f"media_proxy -t {rx_mp_port} -r {rx_rdma_ip} -p {rx_rdma_port_range}", cwd=".")
     time.sleep(0.2) # short sleep used for media_proxy to spin up
     if receiver_mp_proc.process.returncode:
         logging.debug(f"receiver media_proxy return code: {receiver_mp_proc.returncode} of type {type(receiver_mp_proc.returncode)}")
--- tests/validation/functional/cluster/video/test_video.py
+++ tests/validation/functional/cluster/video/test_video.py
@@ -16,8 +16,16 @@ from Engine.media_files import yuv_files
 @pytest.mark.parametrize("video_type", [k for k in yuv_files.keys()])
 def test_video(build_TestApp, build: str, media_proxy_cluster, media: str, video_type: str, tx_mp_port:int = 8002, rx_mp_port: int = 8003) -> None:
     media_proxy_cluster
-    client = Engine.client_json.ClientJson()
-    conn_mpg = Engine.connection.Rdma()
+
+    rx_client = Engine.client_json.ClientJson(apiConnectionString="Server=127.0.0.1; Port=8002")
+    rx_client_filename = "rx_client.json"
+    utils.create_client_json(build, rx_client, rx_client_filename)
+
+    tx_client = Engine.client_json.ClientJson(apiConnectionString="Server=127.0.0.1; Port=8003")
+    tx_client_filename = "tx_client.json"
+    utils.create_client_json(build, tx_client, tx_client_filename)
+
+    conn_mpg = Engine.connection.MultipointGroup(urn="rdma1") # FIXME: Temporary workaround for SDBQ-1949
     payload = Engine.payload.Video(
         width=yuv_files[video_type]["width"],
         height=yuv_files[video_type]["height"],
@@ -28,7 +36,6 @@ def test_video(build_TestApp, build: str, media_proxy_cluster, media: str, video
         connection=conn_mpg, payload=payload
     )
 
-    utils.create_client_json(build, client)
     utils.create_connection_json(build, connection)
 
     # Use a specified file from media_files.py
@@ -48,5 +55,7 @@ def test_video(build_TestApp, build: str, media_proxy_cluster, media: str, video
         timeout=0,
         media_info=media_info,
         rx_mp_port=rx_mp_port,
-        tx_mp_port=tx_mp_port
+        tx_mp_port=tx_mp_port,
+        rx_client_filename = rx_client_filename,
+        tx_client_filename = tx_client_filename,
         )
