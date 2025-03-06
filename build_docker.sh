982b7063e799f1016c4bc4aabb3eb4f8a5dcc034
Magda-Pytel <magdalena.pytel@intel.com>
2025-03-06 09:29:38 +0100

    Deploy mesh-agent (#341)
    Add: target for building mesh-agent in Dockerfile
    Add: new target buildx command in build_docker.sh script
    Add: readme with instructions how to build and run an image of mesh agent
    ---------
    Signed-off-by: Magda-Pytel <magdalena.pytel@intel.com>
    Signed-off-by: Wilczynski, Andrzej <andrzej.wilczynski@intel.com>
    Co-authored-by: Mateusz Grabuszyński <mgrabuszynski@gmail.com>

--- build_docker.sh
+++ build_docker.sh
@@ -17,7 +17,8 @@ mapfile -t BUILD_ARGUMENTS < <(compgen -e | sed -nE '/^(.*)(_proxy|_PROXY)$/{s/^
 BUILD_ARGUMENTS+=( "--progress=plain" "--network=host" "--build-arg" "IMAGE_CACHE_REGISTRY=${IMAGE_CACHE_REGISTRY}" )
 
 docker buildx build "${BUILD_ARGUMENTS[@]}" -t "${IMAGE_REGISTRY}/mcm/sdk:${IMAGE_TAG}" -f "${SCRIPT_DIR}/sdk/Dockerfile" "$@" "${SCRIPT_DIR}"
-docker buildx build "${BUILD_ARGUMENTS[@]}" -t "${IMAGE_REGISTRY}/mcm/media-proxy:${IMAGE_TAG}" -f "${SCRIPT_DIR}/media-proxy/Dockerfile" "$@" "${SCRIPT_DIR}"
+docker buildx build "${BUILD_ARGUMENTS[@]}" -t "${IMAGE_REGISTRY}/mcm/media-proxy:${IMAGE_TAG}" -f "${SCRIPT_DIR}/media-proxy/Dockerfile" "$@" "${SCRIPT_DIR}" --target media-proxy
+docker buildx build "${BUILD_ARGUMENTS[@]}" -t "mcm/mesh-agent:${IMAGE_TAG}" -f "${SCRIPT_DIR}/media-proxy/Dockerfile" "$@" "${SCRIPT_DIR}" --target mesh-agent
 docker buildx build "${BUILD_ARGUMENTS[@]}" --build-arg FFMPEG_VER="7.0" -t "${IMAGE_REGISTRY}/mcm/ffmpeg:${IMAGE_TAG}" -f "${SCRIPT_DIR}/ffmpeg-plugin/Dockerfile" "$@" "${SCRIPT_DIR}"
 docker buildx build "${BUILD_ARGUMENTS[@]}" --build-arg FFMPEG_VER="6.1" -t "${IMAGE_REGISTRY}/mcm/ffmpeg:6.1-${IMAGE_TAG}" -f "${SCRIPT_DIR}/ffmpeg-plugin/Dockerfile" "$@" "${SCRIPT_DIR}"
 
