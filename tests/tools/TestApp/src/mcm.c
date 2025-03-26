b84fec36a5eb15c1f886017a799ce20e33f5e3bf
zLukas <lukasz.grab@intel.com>
2025-03-26 14:51:07 +0000

    remove unnecessary code

--- tests/tools/TestApp/src/mcm.c
+++ tests/tools/TestApp/src/mcm.c
@@ -76,7 +76,6 @@ int mcm_send_video_frames(MeshConnection *connection, const char *filename,
         clock_gettime(CLOCK_REALTIME, &ts_frame_end);
         elapsed = 1000000 * (ts_frame_end.tv_sec - ts_frame_begin.tv_sec) +
                   (ts_frame_end.tv_nsec - ts_frame_begin.tv_nsec) / 1000;
-        LOG("elapsed: %li; sleep_us: %li\n", elapsed, sleep_us);
         if (sleep_us > elapsed) {
             usleep(sleep_us - elapsed);
             LOG("[TX] Elapsed: %li; Slept: %li", elapsed, sleep_us - elapsed);
