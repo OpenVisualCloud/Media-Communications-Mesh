467252e68dc5d441dc785443e9391912d2bffe5e
KarolinaPomian <karolina.agnieszka.castellanos.castillo@intel.com>
2025-07-16 12:26:07 +0000

    add Engine code plus conftest

---
+++ tests/validation/Engine/rx_tx_app_client_json.py
@@ -0,0 +1,45 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2024-2025 Intel Corporation
+# Media Communications Mesh
+
+import json
+
+
+class ClientJson:
+    """Class used to create client.json configuration file"""
+
+    def __init__(
+        self,
+        host,
+        apiVersion="v1",
+        apiConnectionString="Server=127.0.0.1; Port=8002",
+        apiDefaultTimeoutMicroseconds=100000,
+        maxMediaConnections=32,
+    ):
+        self.host = host
+        self.apiVersion = apiVersion
+        self.apiConnectionString = apiConnectionString
+        self.apiDefaultTimeoutMicroseconds = apiDefaultTimeoutMicroseconds
+        self.maxMediaConnections = maxMediaConnections
+
+    def set_client(self, edits: dict) -> None:
+        self.apiVersion = edits.get("apiVersion", self.apiVersion)
+        self.apiConnectionString = edits.get("apiConnectionString", self.apiConnectionString)
+        self.apiDefaultTimeoutMicroseconds = edits.get(
+            "apiDefaultTimeoutMicroseconds", self.apiDefaultTimeoutMicroseconds
+        )
+        self.maxMediaConnections = edits.get("maxMediaConnections", self.maxMediaConnections)
+
+    def to_json(self) -> str:
+        json_dict = {
+            "apiVersion": self.apiVersion,
+            "apiConnectionString": self.apiConnectionString,
+            "apiDefaultTimeoutMicroseconds": self.apiDefaultTimeoutMicroseconds,
+            "maxMediaConnections": self.maxMediaConnections,
+        }
+        return json.dumps(json_dict, indent=4)
+
+    def prepare_and_save_json(self, output_path: str = "client.json") -> None:
+        f = self.host.connection.path(output_path)
+        json_content = self.to_json().replace('"', '\\"')
+        f.write_text(json_content)
---
+++ tests/validation/Engine/rx_tx_app_payload_usage_example.py
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2024-2025 Intel Corporation
+# Media Communications Mesh
+
+from mcm.Engine.rx_tx_app_payload import Audio, Video
+
+payload = Video(width=3840, height=2160)
+
+print(
+    f"""payload info:
+width={payload.width}
+height={payload.height}
+fps={payload.fps}
+pixelFormat={payload.pixelFormat}
+type={payload.payload_type}
+"""
+)
+
+print(
+    f"""Dict:
+{payload.to_dict()}
+"""
+)
+
+
+pl = Audio(channels=4)
+
+print(
+    f"""pl info:
+channels={pl.channels}
+type={pl.payload_type}
+"""
+)
+
+print(
+    f"""Dict:
+{pl.to_dict()}
+"""
+)
